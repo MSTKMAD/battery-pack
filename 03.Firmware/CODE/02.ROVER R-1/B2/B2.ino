18ad0a017d49bfb6bec19c15c63aca6c73225581
Jmusotoku <javier@musotoku.com>
2024-01-30 13:25:11 +0100

    Correcion de espaciados

--- 03.Firmware/CODE/02.ROVER R-1/B2/B2.ino
+++ 03.Firmware/CODE/02.ROVER R-1/B2/B2.ino
@@ -14,7 +14,7 @@
 
 #define MAX_VOLTAGE 120
 #define MIN_VOLTAGE 50
-#define SERIAL_DEBUG
+// #define SERIAL_DEBUG
 //============================================================== PINES ===========================================================//
 const uint16_t C_PIN_ENABLE_LDO_VCC_2 = 1; // Enable del LDO de la alimentacion de VCC_2
 const uint16_t C_PIN_OP_SWITCH = 13;       // Señal que activa/desactiva el transistor de salida en la placa DCDC. HIHG = ON, LOW = OFF
@@ -160,28 +160,28 @@ HealthMonitor short_current_protection(C_LIMIT_SHORTCIRCUIT_PROT, 10, 1, 200);
 dcdc_controler DCDC(C_PIN_EN_DCDC);
 
 //--------------------------------------- Timers variables-------------------------------------
-MilliTimer protection_event_delay;       // Delay impuesto entre detecciones de erroes.
-MilliTimer timer_sec_count;              // Timer contador de segundos.
-MilliTimer timer_display_capacity;       // Timer para el muestreo de la capacidad en el arranque.
-MilliTimer timer_recover_voltage;        // Timer que controla el tiempo que se deja para que la bateria se recupere antes de realizar una medicion de capacidad.
-MilliTimer timer_wait_sleep;             // Timer que controla el tiempo entre los checkeos del boton central durante el protocolo de apagado.
-MilliTimer timer_irq_button_center;      // Timer que controla el tiempo tras el cual, si no hay eventos que saquen al sistema de SLEEP, el sistema vuelve al estado de bajo consumo.
-MilliTimer timer_idle;                   // Timer que controla el tiempo de inactividad sobre el sistema.
-MilliTimer timer_display_error;          // Timer que controla el tiempo que se muestra por pantalla el aviso de error.
-MilliTimer timer_init_screen;            // Timer que controla el tiempo que se muestra el logo durante el encendido.
-MilliTimer timer_end_screen;             // Timer que controla el tiempo que se muestra la pantalla de apagado.
-MilliTimer timer_refresh_screen;         // Timer que control el tiempo tras el cual se refresca la infomracion de la pantalla durante el estadado de Run y Stop.
-MilliTimer timer_log_sec;                // Timer que cuenta hasta un seg para el logeo temporal.
-MilliTimer timer_log_active;             // Timer que controla el intervalo de tiempo que tiene que pasar el sistema en el estado de RUN para realizar un log de ciertas variables de estado.
-MilliTimer timer_check_diagnostic;       // Timer que controla la ventana de tiempo durante la que se comprueba los eventos necesarios para entrar en modo diagnostico.
-MilliTimer timer_waiting_naming;         // Timer que controla la ventana de tiempo durante la que se comprueba los eventos necesarios para entrar en la configuracion del naming.
-MilliTimer timer_active_naming;          // Timer que controla el tiempo que tiene que manterse activo los eventos necesarios para entrar en la configuracion del naming.
-MilliTimer timer_blink_error;            // Periodo del parpadeo de la barra de potencia durante el aviso de un error.
-MilliTimer timer_test_op_switch;         // Timer que durante el modo testeo invierte la señal de op switch.
-MilliTimer timer_test_en_dcdc;           // Timer que durante el modo testeo invierte la señal de en dcdc.
-MilliTimer timer_test_dac;               // Timer que durante el modo testeo invierte la señal de en dac.
-MilliTimer timer_test_sensing;           // Timer que controla el periodo de muestreo durante el modo de test.
-MilliTimer timer_enter_menu;             // Timer que controla el tiempo para entrar en el menu de configuracion.
+MilliTimer protection_event_delay;  // Delay impuesto entre detecciones de erroes.
+MilliTimer timer_sec_count;         // Timer contador de segundos.
+MilliTimer timer_display_capacity;  // Timer para el muestreo de la capacidad en el arranque.
+MilliTimer timer_recover_voltage;   // Timer que controla el tiempo que se deja para que la bateria se recupere antes de realizar una medicion de capacidad.
+MilliTimer timer_wait_sleep;        // Timer que controla el tiempo entre los checkeos del boton central durante el protocolo de apagado.
+MilliTimer timer_irq_button_center; // Timer que controla el tiempo tras el cual, si no hay eventos que saquen al sistema de SLEEP, el sistema vuelve al estado de bajo consumo.
+MilliTimer timer_idle;              // Timer que controla el tiempo de inactividad sobre el sistema.
+MilliTimer timer_display_error;     // Timer que controla el tiempo que se muestra por pantalla el aviso de error.
+MilliTimer timer_init_screen;       // Timer que controla el tiempo que se muestra el logo durante el encendido.
+MilliTimer timer_end_screen;        // Timer que controla el tiempo que se muestra la pantalla de apagado.
+MilliTimer timer_refresh_screen;    // Timer que control el tiempo tras el cual se refresca la infomracion de la pantalla durante el estadado de Run y Stop.
+MilliTimer timer_log_sec;           // Timer que cuenta hasta un seg para el logeo temporal.
+MilliTimer timer_log_active;        // Timer que controla el intervalo de tiempo que tiene que pasar el sistema en el estado de RUN para realizar un log de ciertas variables de estado.
+MilliTimer timer_check_diagnostic;  // Timer que controla la ventana de tiempo durante la que se comprueba los eventos necesarios para entrar en modo diagnostico.
+MilliTimer timer_waiting_naming;    // Timer que controla la ventana de tiempo durante la que se comprueba los eventos necesarios para entrar en la configuracion del naming.
+MilliTimer timer_active_naming;     // Timer que controla el tiempo que tiene que manterse activo los eventos necesarios para entrar en la configuracion del naming.
+MilliTimer timer_blink_error;       // Periodo del parpadeo de la barra de potencia durante el aviso de un error.
+MilliTimer timer_test_op_switch;    // Timer que durante el modo testeo invierte la señal de op switch.
+MilliTimer timer_test_en_dcdc;      // Timer que durante el modo testeo invierte la señal de en dcdc.
+MilliTimer timer_test_dac;          // Timer que durante el modo testeo invierte la señal de en dac.
+MilliTimer timer_test_sensing;      // Timer que controla el periodo de muestreo durante el modo de test.
+MilliTimer timer_enter_menu;        // Timer que controla el tiempo para entrar en el menu de configuracion.
 
 //--------------------------------------- States variables-------------------------------------
 int16_t sw_status = C_SW_ST_SLEEP;                                                   // Identificador del estado del sistema
@@ -1689,10 +1689,6 @@ void setup()
                     /* Output */
                     sw_output = C_OUTPUT_ON;
                     user_output = C_OUTPUT_ON; // Activacion de la salida.
-                    if (nitro_status == false)
-                    {
-                        timer_gap_arranque_nitro_off.set(C_TIME_GAP_ARRANQUE_NITRO_OFF);
-                    }
                     /* Clear Flags */
                     flag_enable_off = false;
 /* Change-State Effects */
