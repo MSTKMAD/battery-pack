6f88f3fb7627328513bc7ae18ed10f277c369bd9
MSTK Javi <javier@musotoku.com>
2025-07-24 08:29:09 +0200

    Test Display042

--- 03.Firmware/LIBRARIES/Adafruit_SSD1306_96/Adafruit_SSD1306_096.h
+++ 03.Firmware/LIBRARIES/Adafruit_SSD1306_96/Adafruit_SSD1306_096.h
@@ -25,9 +25,10 @@
 #define _Adafruit_SSD1306_H_
 
 // ONE of the following three lines must be #defined:
-//#define SSD1306_128_64 ///< DEPRECTAED: old way to specify 128x64 screen
-#define SSD1306_128_32 ///< DEPRECATED: old way to specify 128x32 screen
+#define SSD1306_128_64 ///< DEPRECTAED: old way to specify 128x64 screen
+//#define SSD1306_128_32 ///< DEPRECATED: old way to specify 128x32 screen
 //#define SSD1306_96_16  ///< DEPRECATED: old way to specify 96x16 screen
+//#define SSD1306_70_40 ///< DEPRECATED: old way to specify 128x32 screen
 // This establishes the screen dimensions in old Adafruit_SSD1306 sketches
 // (NEW CODE SHOULD IGNORE THIS, USE THE CONSTRUCTORS THAT ACCEPT WIDTH
 // AND HEIGHT ARGUMENTS).
@@ -121,7 +122,10 @@ typedef uint32_t PortMask;
 #define SSD1306_LCDWIDTH 96  ///< DEPRECATED: width w/SSD1306_96_16 defined
 #define SSD1306_LCDHEIGHT 16 ///< DEPRECATED: height w/SSD1306_96_16 defined
 #endif
-
+#if defined SSD1306_70_40
+#define SSD1306_LCDWIDTH 70  ///< DEPRECATED: width w/SSD1306_70_40 defined
+#define SSD1306_LCDHEIGHT 40 ///< DEPRECATED: height w/SSD1306_70_40 defined
+#endif
 /*!
     @brief  Class that stores state and functions for interacting with
             SSD1306 OLED displays.
